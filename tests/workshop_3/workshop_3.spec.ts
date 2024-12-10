c8b11f9fc89177f00438289687d51d62ad0bb3a6
Svitlana Kyrychenko <ss.kirichenko@gmail.com>
2024-12-10 23:18:53 +0100

    dblclick and right click

--- tests/workshop_3/workshop_3.spec.ts
+++ tests/workshop_3/workshop_3.spec.ts
@@ -1,8 +1,15 @@
 import { test, expect } from '@playwright/test'
 
-test.only('Advanced interaction', async({page}) => {
+test('Advanced interaction', async({page}) => {
     await page.goto('file:///Users/svitlana/Playwright%20course%20by%20Shpak/tests/workshop_3/index.html')
     await page.locator('#hover-me').hover()
     expect( await page.locator('#hover-me')).toHaveText('Text Changed!')
-   
+    
+    await page.locator('#context-menu').click({button: 'right'})
+    expect( await page.getByText('Context Menu Appears!')).toBeVisible()
+    
+    await page.locator('#double-click').dblclick()
+    //expect(await page.getByText('Cute cat')).toBeVisible()
+    expect(await page.getByAltText('Cute Cat').count()).toBe(1)
+
 })
